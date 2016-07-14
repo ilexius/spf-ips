9682cbcf93737ebf783ecff79bbf22811f2aa6b5
Thomas Bach <t.bach@ilexius.de>
2016-07-14 12:08:26 +0200

    only test the whole parser from this point on

--- tests/Test/Parser/SPF.hs
+++ tests/Test/Parser/SPF.hs
@@ -1,13 +1,11 @@
 {-# LANGUAGE TemplateHaskell #-}
-module Test.Parser.SPF where
+module Test.Parser.SPF (tests) where
 
+import Data.ByteString.Char8 (pack)
+import Data.Attoparsec.ByteString (parseOnly)
+import Control.Lens (set)
 import Test.Tasty.TH
 import Test.Tasty.HUnit
-
-import Data.ByteString (empty)
-import Data.ByteString.Char8 (pack)
-import Data.Attoparsec.ByteString
-import Control.Lens (set)
 
 import Data.SPF
 import Parser.SPF
@@ -18,16 +16,6 @@ testParser parser str exp = case parseOnly parser packedStr of
                               Right result -> result @?= exp 
                               x -> assertFailure $ "Expected Right got: " ++ show x
   where packedStr = pack str
-
-case_spf_version = testParser (pSpf1Start <* endOfInput) "v=spf1" defaultSpf1Data
-
-case_simple_include = testParser (pInclude <* endOfInput) inclStr (pack domain)
-  where inclStr = "include:" ++ domain
-        domain = "spf-a.outlook.com"
-
-case_simple_ip4 = testParser (pIp4 <* endOfInput) src (pack ip4)
-  where src = "ip4:" ++ ip4
-        ip4 = "157.55.9.128/25"
 
 case_whole_spf_data = testParser (pSpf) src expected
   where src = unwords [ "v=spf1 include:spf-a.outlook.com include:spf-b.outlook.com"
