22310728780e613400d65d9d5425304d1e5631a1
Thomas Bach <t.bach@ilexius.de>
2016-07-14 12:47:07 +0200

    removed redundant brackets

--- tests/Test/Parser/SPF.hs
+++ tests/Test/Parser/SPF.hs
@@ -17,7 +17,7 @@ testParser parser str exp = case parseOnly parser packedStr of
                               x -> assertFailure $ "Expected Right got: " ++ show x
   where packedStr = pack str
 
-case_whole_spf_data = testParser (pSpf) src expected
+case_whole_spf_data = testParser pSpf src expected
   where src = unwords [ "v=spf1 include:spf-a.outlook.com include:spf-b.outlook.com"
                       , "ip4:157.55.9.128/25 include:spf.protection.outlook.com"
                       , "include:spf-a.hotmail.com include:_spf-ssg-b.microsoft.com"
@@ -36,10 +36,10 @@ case_whole_spf_data = testParser (pSpf) src expected
                      [pack "157.55.9.128/25" ]
                      []
 
-case_spf_data_w_ip6 = testParser (pSpf) src expected
+case_spf_data_w_ip6 = testParser pSpf src expected
   where src = "v=spf1 ip6:2a01:111:f400::/48 ip4:23.103.128.0/19"
         expected = SpfData 1 Nothing [] [pack "23.103.128.0/19"] [pack "2a01:111:f400::/48"]
 
-case_spf_data_w_redirect = testParser (pSpf) src expected
+case_spf_data_w_redirect = testParser pSpf src expected
   where src = "v=spf1 redirect=_spf.google.com"
         expected = set redirect (Just . pack $ "_spf.google.com") defaultSpf1Data
